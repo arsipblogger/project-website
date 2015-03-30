22baf2a94acdea0cd11a84483f0406b4ff3a8232
yudisupriyadi123 <yudisupriyadi123@gmail.com>
2015-03-31 05:35:08 +0700

    My Commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+$title = 'Site Title';
+$description = 'Site Description';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Just a Title</title>
+</head>
+<body>
+
+<h1><?php $title; ?></h1>
+<h5><?php $description; ?></h5>
+
+</body>
+</html>
